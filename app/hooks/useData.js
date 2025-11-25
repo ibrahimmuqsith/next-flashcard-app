44e9764a0849fe4ecdb2e48e280dc8419f3cd8af
Muqsith Ibrahim <ibrahimmuqsith@gmail.com>
2025-11-25 09:47:33 +0530

    using mock data due to 429 error

--- app/hooks/useData.js
+++ app/hooks/useData.js
@@ -1,5 +1,6 @@
 
 import { useEffect, useState } from "react"
+import { ALL_QUESTION } from "../mocks/mockData"
 
 const useData = (endpoint) => {
     const [data, setData] = useState([])
@@ -13,13 +14,14 @@ const useData = (endpoint) => {
         const fetchData = async () => {
             setLoading(true)
             try {
-                const resp = await fetch(endpoint, { signal })
-                if (!resp.ok) {
-                    throw new Error(`http err, status: ${resp.status}`)
-                }
-                console.log("useData resp -->> ", resp)
-                const jsonResp = await resp.json()
-                setData(jsonResp)
+                setData(ALL_QUESTION)
+                // const resp = await fetch(endpoint, { signal })
+                // if (!resp.ok) {
+                //     throw new Error(`http err status: ${resp.status}`)
+                // }
+                // console.log("useData resp -->> ", resp)
+                // const jsonResp = await resp.json()
+                // setData(jsonResp)
             } catch (err) {
                 console.error(err)
             }
